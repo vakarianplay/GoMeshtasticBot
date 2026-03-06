158a7f696f5954756d59fc6495488de4e3a8305a
Kostya Vakarian <vakarianplay@gmail.com>
2026-03-06 12:36:50 +0300

    Inital commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module mesh
+
+go 1.25.0
+
+require github.com/lmatte7/gomesh v0.2.1
+
+require (
+	github.com/jacobsa/go-serial v0.0.0-20180131005756-15cf729a72d4 // indirect
+	golang.org/x/sys v0.0.0-20210525143221-35b2ab0089ea // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/lmatte7/gomesh"
+	pb "github.com/lmatte7/gomesh/github.com/meshtastic/gomeshproto"
+)
+
+const nodeAddress = "192.168.88.49"
+
+func main() {
+	var radio gomesh.Radio
+
+	if err := radio.Init(nodeAddress); err != nil {
+		log.Fatal(err)
+	}
+	defer radio.Close()
+
+	responses, err := radio.GetRadioInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var myNum uint32
+	var myInfo *pb.FromRadio_MyInfo
+	var myNode *pb.NodeInfo
+
+	for _, r := range responses {
+		if info, ok := r.GetPayloadVariant().(*pb.FromRadio_MyInfo); ok {
+			myInfo = info
+			myNum = info.MyInfo.MyNodeNum
+		}
+		if ni, ok := r.GetPayloadVariant().(*pb.FromRadio_NodeInfo); ok {
+			if myNum != 0 && ni.NodeInfo.Num == myNum {
+				myNode = ni.NodeInfo
+				fmt.Printf("ni.NodeInfo.Channel: %v\n", ni.NodeInfo.Channel)
+			}
+		}
+	}
+
+	var metrics string
+	var name string
+	var userId string
+	if myNode != nil && myNode.User != nil && myNode.User.LongName != "" {
+		name = myNode.User.LongName
+		userId = myNode.User.Id
+		metrics = myNode.DeviceMetrics.String()
+	} else if myNode != nil && myNode.User != nil {
+		name = myNode.User.ShortName
+
+	}
+
+	fmt.Println("Node num: ", myNum)
+	fmt.Println("ID: ", userId)
+	fmt.Println("Name: ", name)
+	fmt.Println("Metrics: ", metrics)
+	fmt.Println("Node info: ", myInfo.MyInfo.String())
+}
